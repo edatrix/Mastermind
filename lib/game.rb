b0c34d959b0bb8d1eaf2904a9ac021e4e7d2d549
Chris Luhring <cluhring@gmail.com>
2014-11-12 12:21:54 -0700

    wired in code generator and secret sequence checkers to game class?

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
   attr_reader :guess,
-              :turns,
+              # :turns,
               :secret_sequence,
               :printer,
               :command,
@@ -10,9 +10,9 @@ class Game
   def initialize(instream, outstream, printer)
     @guess           = 1
     # @turns           = 1
-    @secret_sequence = Random.rand(0..1000)
+    @secret_sequence = []
     @printer         = printer
-    @command         = ""
+    @command         = []
     @instream        = instream
     @outstream       = outstream
     @start_time      = Time.now
@@ -32,6 +32,12 @@ class Game
   end
 
   private
+
+                                # => nil
+  def secret_sequence                              # => []
+    4.times {@secret_sequence << ["r", "g", "b", "y"].sample}  # => 4
+    # puts @secret_sequence.join                                 # => nil
+  end
 
   def process_game_turn
     case
@@ -78,6 +84,53 @@ class Game
 
   def game_length
     @end_time - @start_time.round
+  end
+
+  def correct_positions
+    strand1 = @user_input.split(//)
+    strand2 = @secret_sequence.split(//)
+    tc1 = strand1[0] <=> strand2[0]
+    tc2 = strand1[1] <=> strand2[1]
+    tc3 = strand1[2] <=> strand2[2]
+    tc4 = strand1[3] <=> strand2[3]
+    total_correct = tc1 + tc2 + tc3 + tc4
+
+    if total_correct == 0
+      puts 4
+    elsif total_correct == -1
+      puts 3
+    elsif total_correct == -2
+      puts 2
+    elsif total_correct == -3
+      puts 1
+    else total_correct == -4
+      puts 0
+    end
+  end
+
+  def correct_colors
+    color1 = @user_input.pop  # => "r"
+    color2 = @user_input.pop  # => "y"
+    color3 = @user_input.pop  # => "b"
+    color4 = @user_input.pop  # => "o"
+
+  if @secret_sequence.include?(color1)                        # => false
+      @secret_sequence.delete_at(@secret_sequence.index(color1))
+    end                                                         # => nil
+
+  if @secret_sequence.include?(color2)                        # => true
+      @secret_sequence.delete_at(@secret_sequence.index(color2))  # => "y"
+    end                                                         # => "y"
+
+  if @secret_sequence.include?(color3)                        # => true
+      @secret_sequence.delete_at(@secret_sequence.index(color3))  # => "b"
+    end                                                         # => "b"
+
+  if @secret_sequence.include?(color4)                        # => false
+      @secret_sequence.delete_at(@secret_sequence.index(color4))
+    end                                                         # => nil
+
+  correct_colors = 4 - @secret_sequence.count  # => 2
   end
 
 end
